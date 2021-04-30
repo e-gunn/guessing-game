c49bf49c8abb33f46506b47bdd0660b68d49a4c5
e-gunn <gunn816@hotmail.com>
2021-04-29 23:22:52 -0700

    Update game progress

--- js/script.js
+++ js/script.js
@@ -59,6 +59,39 @@ const makeGuess = function (guess) {
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        pageUpdate();
+        UpdateWordInProgress(guessedLetters);
     }
 };
 
+const pageUpdate = function () {
+    guessedLettersList.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersList.append(li);
+    }
+};
+
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            revealWord.push(letter.toUpperCase());
+        } else {
+            revealWord.push("●");
+        }
+    }
+    // console.log(wordArray);
+    UpdateWordInProgress.innerText = revealWord.join("");
+    successfulGuess();
+};
+
+const successfulGuess = function () {
+    if (word.toUpperCase() === UpdateWordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerText = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
+    }
+};
